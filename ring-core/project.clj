441ac18d29364de30fff4996249f37414d7a05fa
Ali Honarvar <honarvar@gmail.com>
2010-11-04 00:05:55 -0400

    an attempt to bring to clojure-1.3.0-SNAPSHOT

--- ring-core/project.clj
+++ ring-core/project.clj
@@ -1,9 +1,11 @@
 (defproject ring/ring-core "0.3.3"
   :description "Ring core libraries."
   :url "http://github.com/mmcgrana/ring"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [;; [org.clojure/clojure "1.2.0"]
+		 [org.clojure/clojure "1.3.0-master-SNAPSHOT"]
                  [commons-codec "1.4"]
                  [commons-io "1.4"]
                  [commons-fileupload "1.2.1"]
                  [javax.servlet/servlet-api "2.5"]]
-  :dev-dependencies [[org.clojure/clojure-contrib "1.2.0"]])
+  :dev-dependencies [;; [org.clojure/clojure-contrib "1.2.0"]
+		     [org.clojure.contrib/standalone "1.3.0-SNAPSHOT"]])
